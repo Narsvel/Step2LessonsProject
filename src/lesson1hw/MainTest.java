057dc12aae2fbc6f80fb5ab092ddfb2f7e618214
Vyacheslav Ostrovsky <narsvel@mail.ru>
2025-04-10 23:36:27 +0300

    Commit 3 add methods get and remove + test in MainTest.java

--- src/lesson1hw/MainTest.java
+++ src/lesson1hw/MainTest.java
@@ -6,11 +6,21 @@ public class MainTest {
 
         MyHashMapRealization<Integer, String> myHashMap = new MyHashMapRealization<>(40);
 
+        System.out.println(myHashMap.isEmpty());
+
         for (int i = 0; i < 100; i++) {
             myHashMap.put(i, "Значение" + i);
         }
 
         System.out.println(myHashMap);
+        System.out.println(myHashMap.size());
+        System.out.println(myHashMap.isEmpty());
+
+        System.out.println(myHashMap.get(30));
+
+        System.out.println(myHashMap.remove(28));
+        System.out.println(myHashMap.size());
+        System.out.println(myHashMap.get(28));
 
     }
 }
--- src/lesson1hw/MyHashMapRealization.java
+++ src/lesson1hw/MyHashMapRealization.java
@@ -1,4 +1,4 @@
-package lesson1hw; //get, put, remove
+package lesson1hw;
 
 public class MyHashMapRealization<Key, Value> {
 
@@ -62,6 +62,8 @@ public class MyHashMapRealization<Key, Value> {
         return size == 0;
     }  //проверка пустой ли MyHashMapRealization
 
+    public int size() { return size; } //возвращает размер MyHashMapRealization
+
     private int keyHash(Key key) { //вычисляем hash ключа
         return (key == null) ? 0 : key.hashCode(); //если ключ равен null возвращаем 0
     }
@@ -71,14 +73,15 @@ public class MyHashMapRealization<Key, Value> {
         if (arrayBucket[keyHash % arrayBucket.length] != null) {    //если в ячейке масива есть Bucket
             Bucket bucket = arrayBucket[keyHash % arrayBucket.length];
             return putInBucket(keyHash, key, value, bucket); //функция для прохода по LinkedList бакетов, сравнения и изменения значений Value
-        } else { //остается проблема в других Bucket массива arrayBucket могут быть другие Classы Key и Value !!!!!!!!!!!!!!!!!!!!
+        } else {
             arrayBucket[keyHash % arrayBucket.length] = new Bucket<>(keyHash, key, value);
             size++;        //увеличиваем размер MyHashMapRealization
             return value;
         }
     }
 
-    private Value putInBucket(int keyHash, Key key, Value value, Bucket bucket) { //функция для прохода по LinkedList бакетов, сравнения и изменения значений Value
+    //функция для рекурсивного прохода по LinkedList бакетов, сравнения и изменения значений Value
+    private Value putInBucket(int keyHash, Key key, Value value, Bucket bucket) {
         if (bucket.getHash() == keyHash  && bucket.getKey().equals(key)) { //если Hash и  Equals true изменяем значение Value в Bucket
             Value oldValue = (Value) bucket.getValue();
             bucket.setValue(value);
@@ -96,24 +99,84 @@ public class MyHashMapRealization<Key, Value> {
         }
     }
 
+    public Value get(Key key) {
+        int keyHash = keyHash(key);
+        if (arrayBucket[keyHash % arrayBucket.length] != null) { //если в ячейке массива есть Bucket
+            Bucket bucket = arrayBucket[keyHash % arrayBucket.length];
+            return getBucketValue(bucket, keyHash, key); //функция для прохода по LinkedList бакетов и сравнения ключей
+        }
+        return null; //если ячейка масива null
+    }
+
+    //функция для рекурсивного прохода по LinkedList бакетов, сравнения ключей и возвращения значения Value если ключи совпали
+    private Value getBucketValue(Bucket bucket, int keyHash, Key key) {
+        if (bucket.getHash() == keyHash  && bucket.getKey().equals(key)) { //сравниваем ключи
+            return (Value) bucket.getValue();                              //возвращаем значение Value если ключи однинаковые
+        } else {
+            if (bucket.getNextBucket() != null) {      //провееряем ссылку на следующий бакет
+                Bucket nextBucket = bucket.nextBucket; //если есть следующий Bucket
+                return getBucketValue(nextBucket, keyHash, key); //рекурсивно вызываем метод getBucketValue
+            }
+        }
+        return null; //если ключ не найден в LinkedList бакетов возвращаем null
+    }
+
+    public Value remove(Key key) {
+        int keyHash = keyHash(key);
+        if (arrayBucket[keyHash % arrayBucket.length] != null) { //если в ячейке массива есть Bucket
+            Bucket bucket = arrayBucket[keyHash % arrayBucket.length];
+            //проверку первого Bucket производим в этом методе т.к. придется изменить ссылку в ячейке массива при совпадении ключей
+                if (bucket.getHash() == keyHash  && bucket.getKey().equals(key)) { //сравниваем ключи
+                    if (bucket.nextBucket != null) {
+                        arrayBucket[keyHash % arrayBucket.length] = bucket.nextBucket;
+                        size--;                                    //уменьшаем размер MyHashMapRealization
+                        return (Value) bucket.getValue();
+                    } else {
+                        arrayBucket[keyHash % arrayBucket.length] = null;
+                        size--;                                    //уменьшаем размер MyHashMapRealization
+                        return (Value) bucket.getValue();
+                    }
+                }
+                if (bucket.nextBucket != null) {                     //если есть следующий бакет в LinkedList
+                    Bucket nextBucket = bucket.nextBucket;           //следующий Bucket пердаем в функцию
+                    return removeBuket(bucket, nextBucket, keyHash, key); //функция для прохода по LinkedList бакетов и сравнения ключей
+                }
+        }
+        return null; //если ячейка масива null
+    }
+
+    //функция для рекурсивного прохода по LinkedList бакетов, сравнения ключей и удаления бакета
+    private Value removeBuket(Bucket beforeBucket, Bucket bucket, int keyHash, Key key) {
+        if (bucket.getHash() == keyHash  && bucket.getKey().equals(key)) { //сравниваем ключи
+            beforeBucket.nextBucket = bucket.getNextBucket();              //меняем ссылку предыдущего бакета
+            size--;                                                        //уменьшаем размер MyHashMapRealization
+            return (Value) bucket.getValue();
+        } else {
+            if (bucket.nextBucket != null) {                               //если есть следующий бакет
+                Bucket nextBucket = bucket.nextBucket;
+                return removeBuket(bucket, nextBucket, keyHash, key);      //рекурсивно вызываем функцию
+            } else return null;
+        }
+    }
+
     @Override
     public String toString() {
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < arrayBucket.length; i++) {
-            Bucket bucket = arrayBucket[i];
-            stringBuilder.append(toStringAddBuckets(bucket));
-            stringBuilder.append("\n");
+        for (int i = 0; i < arrayBucket.length; i++) { //проходим по массиву arrayBucket
+            if (arrayBucket[i] != null) {              //если баккет в ячеке массива не null
+                Bucket bucket = arrayBucket[i];
+                toStringAddBuckets(bucket, stringBuilder); //метод проходит по LinkedList Bucket в ячейке масива и возвращае их значения в одной строке
+                stringBuilder.append("\n");                //перенос строки после прохода по LinkedList Bucket
+            }
         }
         return "MyHashMapRealization( \n" + stringBuilder + ')';
     }
 
-    private String toStringAddBuckets(Bucket bucket) { //метод проходит по Bucket в ячейке масива и возвращае их значения в одной строке
-        StringBuilder stringBuilder = new StringBuilder();
+    private void toStringAddBuckets(Bucket bucket, StringBuilder stringBuilder) { //метод проходит по Bucket в ячейке масива и возвращае их значения в одной строке
         stringBuilder.append(bucket.toString());
         if (bucket.getNextBucket() != null) {
-            stringBuilder.append(toStringAddBuckets(bucket.nextBucket));
+            toStringAddBuckets(bucket.nextBucket, stringBuilder);
         }
-        return stringBuilder.toString();
     }
 
 }
